15270b3971c90676b343c73676c3ac4e83e86741
chaminduindula <89722798+chaminduindula@users.noreply.github.com>
2024-04-07 09:43:12 +0530

    config-env.js

---
+++ config-env.js
@@ -0,0 +1,25 @@
+/**
+
+* @project_name Star-Princess
+* @authors Chamindu Indula <https://github.com/chaminduindula>
+* @description The main hope of creating this bot is to take full advantage of the WhatsApp app and make its work easier
+* @link <https://github.com/chaminduindula/Star-Princess>
+* @version 2.0.6
+
+© 2023 Chamindu Indula, CHAMI-BOT. All rights reserved.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.*/
+
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "Enter your session-id",
+BOT_NUMBER: process.env.BOT_NUMBER || "Enter your botnumber (without + mark and spaces)",
+PASSWORD: process.env.PASSWORD || "Enter you like password",
+GITHUB_USERNAME: process.env.GITHUB_USERNAME || "Enter your github username",
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "Enter your GITHUB_AUTH_TOKEN"
+};
